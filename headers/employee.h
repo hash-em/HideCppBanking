4ce4135869fb20866288f55bf0e59b20b67a3c57
naimothmani <naimothmani24@gmail.com>
2025-12-02 09:41:38 +0100

    Update employee.h

--- headers/employee.h
+++ headers/employee.h
@@ -1,32 +1,16 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
-#include <string>
-using namespace std ;
 
-#include "employee_list.h"
+using namespace std;
 
 struct Employee {
-    int id;
-    string firstName;
+    string id;
+    string name;
     string lastName;
     string address;
     double salary;
     string hireDate;
     string branchCode;
-    Employee* next;
-    Employee() : id(0), next(nullptr) {}
 };
-
-struct employeeList {
-    Employee* head;
-    int size;
-};
-
-
-void initEmployeeSystem();
-bool employeeLogin(int& employeeId);
-int employeeInterface(int employeeId);
-void saveEmployeesToFile();
-void loadEmployeesFromFile();
 
 #endif
