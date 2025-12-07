a01f8e24a19d4d7dffaa315b624176106306935e
Hbibbk <mbenkaddech@gmail.com>
2025-12-07 22:26:25 +0100

    las

---
+++ headers/statstics.h
@@ -0,0 +1,15 @@
+#include "employee.list.h"
+#include "customer_lists.h"
+#include <string>
+
+int numberOfLoans(const customerList& l);
+int loansType(const customerList& l, string type);
+int loansStatus(const customerList& l, string status);
+bool compareDates(string s1, string s2);
+customer highestNbLoans(const customerList& l);
+customer highestBalance(const customerList& l);
+customer lowestBalance(const customerList& l);
+int activeLoans(const customerList& l, string start, string end);
+int numberOfEmployees(const EmployeeArray& l);
+int numEmployeesByBranch(const EmployeeArray& l, string branch);
+void statstics();
